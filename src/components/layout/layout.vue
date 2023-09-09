0489e4a092b12bf132bc5ad3e8ae1f7a394801a8
gaoqi <1224362143@qq.com>
2023-09-09 20:27:44 +0800

    chore: 添加菜单icon

--- src/components/layout/layout.vue
+++ src/components/layout/layout.vue
@@ -6,13 +6,21 @@
       </div>
     </a-layout-header>
     <a-layout>
-      <a-layout-sider width="200" style="background: #fff" class="shadow-lg">
+      <a-layout-sider
+        width="200"
+        style="background: #fff"
+        class="shadow-lg"
+        collapsible
+      >
         <a-menu
           v-model:selectedKeys="activeKey"
           :style="{ height: '100%', borderRight: 0 }"
           mode="vertical"
         >
           <a-sub-menu v-for="item in menus" :key="item.key">
+            <template #icon v-if="item.icon"
+              ><img :src="item.icon" class="w-[20px]"
+            /></template>
             <template #title>
               <span>
                 <user-outlined />
@@ -30,7 +38,7 @@
           </a-sub-menu>
         </a-menu>
       </a-layout-sider>
-      <a-layout style="padding: 0 24px 24px">
+      <a-layout style="padding: 24px">
         <a-layout-content
           :style="{
             background: '#fff',
@@ -62,6 +70,13 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 import { useRouter, RouterLink } from 'vue-router'
+import 订单 from '@/assets/订单.svg'
+import 会员 from '@/assets/会员.svg'
+import 员工 from '@/assets/员工.svg'
+import 角色 from '@/assets/角色.svg'
+import 门店 from '@/assets/门店.svg'
+import 运营 from '@/assets/运营.svg'
+import 设置 from '@/assets/设置.svg'
 const activeKey = ref<string[]>([])
 const matched = ref<Record<string, any>[]>([])
 const routerData = ref<Record<string, any>>({})
@@ -83,13 +98,15 @@ const menus = ref([
       { title: '订单未创建', path: '/order/created', key: 'order-created' },
       { title: '订单已创建', path: '/order/unCreate', key: 'order-unCreate' }
     ],
-    path: '/order'
+    path: '/order',
+    icon: 订单
   },
   {
     title: '会员管理',
     key: 'member',
     children: [{ title: '会员列表', path: '/member/list', key: 'member-list' }],
-    path: '/member'
+    path: '/member',
+    icon: 会员
   },
   {
     title: '员工管理',
@@ -102,19 +119,22 @@ const menus = ref([
         key: 'employee-leave-list'
       }
     ],
-    path: '/employee'
+    path: '/employee',
+    icon: 员工
   },
   {
     title: '角色管理',
     key: 'role',
     children: [{ title: '角色列表', path: '/role/list', key: 'role-list' }],
-    path: '/role'
+    path: '/role',
+    icon: 角色
   },
   {
     title: '门店管理',
     key: 'stores',
     children: [{ title: '门店列表', path: '/stores/list', key: 'stores-list' }],
-    path: '/stores'
+    path: '/stores',
+    icon: 门店
   },
   {
     title: '运营管理',
@@ -122,7 +142,8 @@ const menus = ref([
     children: [
       { title: '充值规则', path: '/operation/list', key: 'operation-list' }
     ],
-    path: '/operation'
+    path: '/operation',
+    icon: 运营
   },
   {
     title: '系统管理',
@@ -134,7 +155,8 @@ const menus = ref([
       { title: '支付方式设置', path: '/pay-type', key: 'pay-type' },
       { title: '营业额标准设置', path: '/turnover', key: 'turnover' }
     ],
-    path: '/operation'
+    path: '/operation',
+    icon: 设置
   }
 ])
 </script>
