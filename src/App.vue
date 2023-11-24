8f2fdae081d729736e95474faf059ff24f0d7ca7
gaoqi <1224362143@qq.com>
2023-11-25 00:21:36 +0800

    chore: vue是垃圾

--- src/App.vue
+++ src/App.vue
@@ -38,6 +38,14 @@ import common from './servers/common'
 import store from '@/store/store'
 import { useRouter } from 'vue-router'
 import router from './route'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+
+// @ts-ignore
+zhCN.DatePicker.lang = {
+  ...zhCN.DatePicker?.lang,
+  monthFormat: 'M月',
+  shortWeekDays: ['日', '一', '二', '三', '四', '五', '六']
+}
 const { dispatch } = store
 onMounted(() => {
   const token = cookie.get('Admin-Token')
